d44254e9377e56a99a1989b6110884b83e37d0ca
sfengxjk <sam.feng@kellogg.northwestern.edu>
2026-03-12 04:25:33 +0000

    Class 8 things done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,8 +9,13 @@ class EntriesController < ApplicationController
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
+  end
+
+  def show
+    @entry = Entry.find_by({"id" => params["id"], "user_id" => session["user_id"]})
   end
 
 end
